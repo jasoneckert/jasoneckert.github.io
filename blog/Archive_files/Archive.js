a65a60ca837d69d75bcae8fa2d7b70b583e17058
Jason Eckert <jason.eckert@trios.com>
2020-02-19 22:20:31 -0500

    feb blog

--- blog/Archive_files/Archive.js
+++ blog/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.2 local-build-20190818
+// Created by iWeb 3.0.2 local-build-20200215
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
---
+++ blog/Entries/2020/2/19_How_to_write_an_academic_technical_textbook_files/19_How_to_write_an_academic_technical_textbook.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.2 local-build-20200219
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,314),url:'19_How_to_write_an_academic_technical_textbook_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_1.png'},{rect:new IWRect(1,-1,245,2),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_2.png'},{rect:new IWRect(246,-1,2,2),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_3.png'},{rect:new IWRect(246,1,2,314),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_4.png'},{rect:new IWRect(246,315,2,3),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_5.png'},{rect:new IWRect(1,315,245,3),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_6.png'},{rect:new IWRect(-1,315,2,3),url:'19_How_to_write_an_academic_technical_textbook_files/stroke_7.png'}],new IWSize(247,316))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('19_How_to_write_an_academic_technical_textbook_files/19_How_to_write_an_academic_technical_textbookMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
