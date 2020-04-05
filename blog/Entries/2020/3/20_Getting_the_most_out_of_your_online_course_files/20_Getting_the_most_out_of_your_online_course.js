ea992666b8aeba0fdea20ad2c5bdc957cc70aa00
Jason Eckert <jason.eckert@trios.com>
2020-04-05 10:46:42 -0400

    Added March and April blogs

---
+++ blog/Entries/2020/3/20_Getting_the_most_out_of_your_online_course_files/20_Getting_the_most_out_of_your_online_course.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20200321
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('20_Getting_the_most_out_of_your_online_course_files/20_Getting_the_most_out_of_your_online_courseMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
