f084edd9f67dd67148359e9abd234b2d03b8e95a
Jason Eckert <jason.eckert@trios.com>
2020-05-07 12:48:16 -0400

    sysrc fixed

--- blog/Entries/2020/5/2_FreeBSD_UNIX_for_Linux_sysadmins_files/2_FreeBSD_UNIX_for_Linux_sysadmins.js
+++ blog/Entries/2020/5/2_FreeBSD_UNIX_for_Linux_sysadmins_files/2_FreeBSD_UNIX_for_Linux_sysadmins.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.2 local-build-20200502
+// Created by iWeb 3.0.2 local-build-20200507
 
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
