65a81212563f3382ef7a9dde15fa6a6e6301b11f
Jason Eckert <jason.eckert@trios.com>
2020-06-02 18:26:13 -0400

    Copyedit blog post

--- blog/Entries/2020/5/18_Setting_up_macOS_for_development_files/18_Setting_up_macOS_for_development.js
+++ blog/Entries/2020/5/18_Setting_up_macOS_for_development_files/18_Setting_up_macOS_for_development.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.2 local-build-20200519
+// Created by iWeb 3.0.2 local-build-20200602
 
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
